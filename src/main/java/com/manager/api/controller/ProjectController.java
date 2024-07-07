efb412e812b6a8c379050c05baf98cb3099d7c7b
Luan Moraes <luansilvamoraess@gmail.com>
2024-07-07 01:01:36 -0300

    feat: creation of the controller and service of the Project entity

--- src/main/java/com/manager/api/controller/ProjectController.java
+++ src/main/java/com/manager/api/controller/ProjectController.java
@@ -1,15 +1,20 @@
 package com.manager.api.controller;
 
+import com.manager.api.domain.project.Project;
+import com.manager.api.service.ProjectService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/projects")
 public class ProjectController {
+    ProjectService projectService;
 
     @GetMapping
-    public String listProjects(){
-        return "Hello World";
+    public List<Project> listProjects(){
+        return projectService.getAllProjects();
     }
 }
--- src/main/java/com/manager/api/service/ProjectService.java
+++ src/main/java/com/manager/api/service/ProjectService.java
@@ -1,7 +1,16 @@
 package com.manager.api.service;
 
+import com.manager.api.domain.project.Project;
+import com.manager.api.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectService {
+    ProjectRepository projectRepository;
+
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
 }
